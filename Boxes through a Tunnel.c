c772f84f5ba41c99ef67593d91d60d506e5776e9
mohith321p <152712274+mohith321p@users.noreply.github.com>
2023-12-23 23:48:26 +0530

    Boxes through a Tunnel.c

--- Boxes through a Tunnel.c
+++ Boxes through a Tunnel.c
@@ -30,8 +30,7 @@ int n;
 scanf("%d", &n);
 box *boxes = malloc(n*sizeof(box));
 for (int i = 0; i < n; i++) {
-scanf("%d%d%d", &boxes[i].length, &boxes[i].width, &boxes[i].
-height);
+scanf("%d%d%d", &boxes[i].length, &boxes[i].width, &boxes[i].height);
 }
 for (int i = 0; i < n; i++) {
 if (is_lower_than_max_height (boxes[i])) {
