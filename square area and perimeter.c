c68a2aeaa011a924462d675f6cdb46c39c9ce8d1
mohith321p <152712274+mohith321p@users.noreply.github.com>
2024-02-19 08:14:02 +0530

    Area and Perimeter

---
+++ square area and perimeter.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ float perimeter,area,s;
+ printf("Enter one side of square: ");
+ scanf("%f",&s);
+ perimeter = 4*s;
+ area=s*s;
+ printf("Perimeter of a square: %.2f\n",perimeter);
+ printf("Area of square: %.2f",area); 	
+}
+
+
