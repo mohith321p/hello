f3001817ed4a6b0cabb54150670b8a3a2556f952
mohith321p <152712274+mohith321p@users.noreply.github.com>
2024-02-19 08:10:07 +0530

    Area

---
+++ Area of Rectangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+ int area,h,w;
+ printf("Enter height of rectangle: ");
+ scanf("%d",&h);
+ printf("Enter width of rectangle: ");
+ scanf("%d",&w);
+ area=h*w;
+ printf("Area of rectangle: %d",area); 	
+}
+
+
