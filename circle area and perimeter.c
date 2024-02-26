52d6ec5fad3527ad6b865b4edd023fc6f50b7392
mohith321p <152712274+mohith321p@users.noreply.github.com>
2024-02-26 22:12:59 +0530

    Area and Perimeter

--- circle area and perimeter.c
+++ circle area and perimeter.c
@@ -4,8 +4,8 @@ int main()
 	float area,perimeter,r;
 	printf("Enter radius of circle: ");
 	scanf("%f",&r);
-    perimeter=2*3.14*r;
+        perimeter=2*3.14*r;
 	area=3.14*r*r;
-    printf("perimeter of circle=%.2f\n",perimeter);
+        printf("perimeter of circle=%.2f\n",perimeter);
 	printf("Area of circle=%.2f",area);
 }
