17f080a51e423e3a19f9e673fbdf05a959565f97
mohith321p <152712274+mohith321p@users.noreply.github.com>
2024-02-26 22:22:12 +0530

    Area and Perimeter

--- triangle area and perimeter.c
+++ triangle area and perimeter.c
@@ -6,8 +6,8 @@ int main()
 	scanf("%f",&b);
 	printf("Enter height: ");
 	scanf("%f",&h);
- perimeter=b+2*h;
+        perimeter=b+2*h;
 	area=0.5*b*h;
- printf("Perimeter of triangle=%.2f\n",perimeter);
+        printf("Perimeter of triangle=%.2f\n",perimeter);
 	printf("Area of triangle=%.2f",area);
 }
