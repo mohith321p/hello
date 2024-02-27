a4ea94b0ce7c85c4424a37e58750c703b9226da7
mohith321p <152712274+mohith321p@users.noreply.github.com>
2024-02-27 07:23:59 +0530

    Conditionals and loops

---
+++ Printing Patterns Using Loops (1).c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h> 
+int min(int, int);
+int main() 
+{
+    	int  i, j, n, length, value;
+        	scanf("%d", &n);
+        	length  = n * 2 - 1;    
+       	for (i = 0; i < length; i++)  
+           {
+                		for (j = 0; j < length; j++) 
+                        {
+                    		value = n - min( min(i,j), min(length-1-i, length-1-j));
+                    		printf("%d ", value);
+                		}
+               		printf("\n");
+        	}
+        	return 0;
+}
+
+int min(int a,int b)
+{
+    if(a<b) return a;
+    else return b;
+}
