286bc4b00dcb78ea0367e074ddebb589f6720a4d
mohith321p <152712274+mohith321p@users.noreply.github.com>
2024-03-06 20:30:49 +0530

    Swapping Techniques

--- Swapping Method 1.c
+++ Swapping Method 1.c
@@ -1,14 +1,14 @@
 #include<stdio.h>
 int main()
 {
-	int a,b,c;
+    int a,b,c;
     printf("Enter two numbers: ");
-    scanf("%d%d",&a,&b);
-	printf("Before swapping: \n");
+    scanf("%d%d",&a,&b)
+    printf("Before swapping: \n");
     printf("a=%d and b=%d\n",a,b);
-	c=a;
-	a=b;
-	b=c;
-printf("After swapping: \n");
-printf("a=%d and b=%d\n",a,b);	
+    c=a;
+    a=b;
+    b=c;
+    printf("After swapping: \n");
+    printf("a=%d and b=%d\n",a,b);	
 }
